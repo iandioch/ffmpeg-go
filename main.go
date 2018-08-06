a3184ac91f07e968a70af3480489fcffabb2db2b
iandioch <iandioch11@gmail.com>
2018-08-06 09:49:15 +0000

    gofmt

--- main.go
+++ main.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os/exec"
-    "strconv"
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strconv"
 )
 
-func extractYoutube(ytURL string, start int, duration int) (string) {
-    s := fmt.Sprintf("%v", start)
-    t := fmt.Sprintf("%v", duration)
+func extractYoutube(ytURL string, start int, duration int) string {
+	s := fmt.Sprintf("%v", start)
+	t := fmt.Sprintf("%v", duration)
 
-    e := exec.Command("ffmpeg", "-t", t, "-ss", s, "-i", ytURL, "-vf", "scale=350:-1", "-an", "-r", "15", "-crf", "24", "banana.gif")
-    fmt.Printf("%s %s\n", e.Path, e.Args)
-    stdout, err := e.CombinedOutput()
-    if err != nil {
-        fmt.Printf("%v\n", string(stdout))
-        panic(err)
-    }
+	e := exec.Command("ffmpeg", "-t", t, "-ss", s, "-i", ytURL, "-vf", "scale=350:-1", "-an", "-r", "15", "-crf", "24", "banana.gif")
+	fmt.Printf("%s %s\n", e.Path, e.Args)
+	stdout, err := e.CombinedOutput()
+	if err != nil {
+		fmt.Printf("%v\n", string(stdout))
+		panic(err)
+	}
 
-    res := string(stdout)
-    fmt.Printf("%s\n", res)
-    return res
+	res := string(stdout)
+	fmt.Printf("%s\n", res)
+	return res
 }
 
 func serveYoutubeExtractor(w http.ResponseWriter, r *http.Request) {
-    ytURL := r.URL.Query().Get("video")
-    start := r.URL.Query().Get("start")
-    dur := r.URL.Query().Get("dur")
+	ytURL := r.URL.Query().Get("video")
+	start := r.URL.Query().Get("start")
+	dur := r.URL.Query().Get("dur")
 
-    startI, err := strconv.Atoi(start)
-    if err != nil {
-        panic(err)
-    }
+	startI, err := strconv.Atoi(start)
+	if err != nil {
+		panic(err)
+	}
 
-    durI, err := strconv.Atoi(dur)
-    if err != nil {
-        panic(err)
-    }
+	durI, err := strconv.Atoi(dur)
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Printf("Received args: %v %v %v\n", ytURL, startI, durI)
+	fmt.Printf("Received args: %v %v %v\n", ytURL, startI, durI)
 
-    res := extractYoutube(ytURL, startI, durI)
+	res := extractYoutube(ytURL, startI, durI)
 
-    w.Write([]byte("response: " +  ytURL + ", " + start + ", " + dur + ", " + res))
+	w.Write([]byte("response: " + ytURL + ", " + start + ", " + dur + ", " + res))
 }
 
 func serveRoot(w http.ResponseWriter, r *http.Request) {
-    message := "Hello world"
-    w.Write([]byte(message))
+	message := "Hello world"
+	w.Write([]byte(message))
 }
 
 func main() {
-    http.HandleFunc("/", serveRoot)
-    http.HandleFunc("/yt_extract", serveYoutubeExtractor)
-    err := http.ListenAndServe(":9008", nil)
-    if err != nil {
-        panic(err)
-    }
+	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/yt_extract", serveYoutubeExtractor)
+	err := http.ListenAndServe(":9008", nil)
+	if err != nil {
+		panic(err)
+	}
 }
