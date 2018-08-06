95f846e06aaf5ee0823a399cb8c2bf4acf01d221
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-08-06 11:13:15 +0300

    Add yt endpoint

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
     "net/http"
 )
 
+func serveYoutubeExtractor(w http.ResponseWriter, r *http.Request) {
+    ytURL := r.URL.Query().Get("video")
+    start := r.URL.Query().Get("start")
+    dur := r.URL.Query().Get("dur")
+
+    // Do ffmpeg stuff
+
+    w.Write([]byte("response: " +  ytURL + ", " + start + ", " + dur))
+}
+
 func serveRoot(w http.ResponseWriter, r *http.Request) {
     message := "Hello world"
     w.Write([]byte(message))
@@ -11,6 +21,7 @@ func serveRoot(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", serveRoot)
+    http.HandleFunc("/yt_extract", serveYoutubeExtractor)
     err := http.ListenAndServe(":9008", nil)
     if err != nil {
         panic(err)
